360a12d97df2629818e3bd626082d36666ad6ecd
thorly <leiyuan1229@outlook.com>
2021-11-29 17:31:01 +0800

    add scripts for Sentinel-1 TOPS phase deramping

---
+++ tintpy/deramp_s1_a.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+################################
+# Program is part of TintPy    #
+# Copyright (c) 2021, Lei Yuan #
+# Author: Lei Yuan, 2021       #
+################################
+
+import argparse
+import os
+import re
+import sys
+
+EXAMPLE = """Example:
+  python3 deramp_s1_a.py /ly/rslc 2 20181229
+  python3 deramp_s1_a.py /ly/rslc 1 2 20181229 -r 8 -a 2
+"""
+
+
+def read_gamma_par(par_file, keyword):
+    value = ''
+    with open(par_file, 'r') as f:
+        for l in f.readlines():
+            if l.count(keyword) == 1:
+                tmp = l.split(':')
+                value = tmp[1].strip()
+    return value
+
+
+def gen_bmp(slc, slc_par, rlks, alks):
+    width = read_gamma_par(slc_par, 'range_samples')
+    bmp = slc + '.bmp'
+    call_str = f"rasSLC {slc} {width} 1 0 {rlks} {alks} 1. .35 1 0 0 {bmp}"
+    os.system(call_str)
+
+
+def write_tab(iw_slc_path, out_file):
+    with open(out_file, 'w+') as f:
+        f.write(
+            f"{iw_slc_path} {iw_slc_path + '.par'} {iw_slc_path + '.tops_par'}\n"
+        )
+
+
+def cmd_line_parser():
+    parser = argparse.ArgumentParser(
+        description=
+        'Calculate and subtract S1 TOPS Doppler phase from burst SLC data after coregistration.',
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=EXAMPLE)
+
+    parser.add_argument('rslc_dir', help='directory path of RSLCs')
+    parser.add_argument('iw_num',
+                        choices=['1', '2', '3'],
+                        nargs='+',
+                        type=str,
+                        help='IW num for deramp')
+    parser.add_argument('ref_slc', help='reference SLC')
+    parser.add_argument('-r',
+                        dest='rlks',
+                        help='range looks',
+                        required=True,
+                        type=int)
+    parser.add_argument('-a',
+                        dest='alks',
+                        help='azimuth looks',
+                        required=True,
+                        type=int)
+    inps = parser.parse_args()
+
+    return inps
+
+
+def main():
+    inps = cmd_line_parser()
+    rslc_dir = inps.rslc_dir
+    iw_num = inps.iw_num
+    iw_num = iw_num.split()
+    rlks = inps.rlks
+    alks = inps.alks
+    ref_slc = inps.ref_slc
+
+    # check rslc_dir
+    rslc_dir = os.path.abspath(rslc_dir)
+    if not os.path.isdir(rslc_dir):
+        sys.exit('Cannot find directory {}'.format(rslc_dir))
+
+    # get all date
+    all_date = sorted(
+        [i for i in os.listdir(rslc_dir) if re.findall(r'^\d{8}$', i)])
+    if len(all_date) < 2:
+        sys.exit('No enough RSLCs.')
+
+    # check ref_slc
+    if re.findall(r'^\d{8}$', ref_slc):
+        if not ref_slc in all_date:
+            sys.exit('No slc for {}.'.format(ref_slc))
+    else:
+        sys.exit('Error date for ref_slc.')
+
+    s_dates = all_date
+    s_dates.remove(ref_slc)
+
+    dates = [ref_slc] + s_dates
+
+    for date in dates:
+        rslc_date_dir = os.path.join(rslc_dir, date)
+        tab = os.path.join(rslc_date_dir, date + '_tab')
+
+        if len(iw_num) == 1:
+            iw_slc = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+
+            write_tab(iw_slc, tab)
+
+        if len(iw_num) == 2:
+            iw_slc1 = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2 = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+
+            write_tab(iw_slc1, tab)
+            write_tab(iw_slc2, tab)
+
+        if len(iw_num) == 3:
+            iw_slc1 = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2 = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+            iw_slc3 = os.path.join(rslc_date_dir, f"{date}.iw{iw_num[2] * 2}.slc")
+
+            write_tab(iw_slc1, tab)
+            write_tab(iw_slc2, tab)
+            write_tab(iw_slc3, tab)
+
+        if date == ref_slc:
+            call_str = f"S1_deramp_TOPS_reference {tab}"
+            os.system(call_str)
+        else:
+            reference_tab = os.path.join(rslc_dir, ref_slc, ref_slc + '_tab')
+            call_str = f"S1_deramp_TOPS_slave {tab} {date} {reference_tab} {rlks} {alks} 0"
+            os.system(call_str)
+
+        rslc_deramp = os.path.join(rslc_date_dir, date + ".rslc.deramp")
+        rslc_deramp_par = rslc_deramp + '.par'
+
+        if date == ref_slc:
+            call_str = f"SLC_mosaic_S1_TOPS {date}_tab.deramp {rslc_deramp} {rslc_deramp_par} {rlks} {alks} 1"
+            os.system(call_str)
+
+        gen_bmp(rslc_deramp, rslc_deramp_par, rlks, alks)
+
+    print('\nAll done, enjoy it!\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ tintpy/deramp_s1_b.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+################################
+# Program is part of TintPy    #
+# Copyright (c) 2021, Lei Yuan #
+# Author: Lei Yuan, 2021       #
+################################
+
+import argparse
+import os
+import re
+import sys
+
+EXAMPLE = """Example:
+  python3 deramp_s1_b.py /ly/slc /ly/slc_deramp 2 -r 8 -a 2
+  python3 deramp_s1_b.py /ly/slc /ly/slc_deramp 1 2 -r 8 -a 2
+"""
+
+
+def read_gamma_par(par_file, keyword):
+    value = ''
+    with open(par_file, 'r') as f:
+        for l in f.readlines():
+            if l.count(keyword) == 1:
+                tmp = l.split(':')
+                value = tmp[1].strip()
+    return value
+
+
+def gen_bmp(slc, slc_par, rlks, alks):
+    width = read_gamma_par(slc_par, 'range_samples')
+    bmp = slc + '.bmp'
+    call_str = f"rasSLC {slc} {width} 1 0 {rlks} {alks} 1. .35 1 0 0 {bmp}"
+    os.system(call_str)
+
+
+def write_tab(iw_slc_path, out_file):
+    with open(out_file, 'w+') as f:
+        f.write(
+            f"{iw_slc_path} {iw_slc_path + '.par'} {iw_slc_path + '.tops_par'}\n"
+        )
+
+
+def cmd_line_parser():
+    parser = argparse.ArgumentParser(
+        description=
+        'Calculate and subtract S1 TOPS Doppler phase from burst SLC data before coregistration.',
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=EXAMPLE)
+
+    parser.add_argument('slc_dir', help='directory path of SLCs')
+    parser.add_argument('out_dir', help='directory path of deramped SLCs')
+    parser.add_argument('iw_num',
+                        choices=['1', '2', '3'],
+                        nargs='+',
+                        type=str,
+                        help='IW num for deramp')
+    parser.add_argument('-r',
+                        dest='rlks',
+                        help='range looks',
+                        required=True,
+                        type=int)
+    parser.add_argument('-a',
+                        dest='alks',
+                        help='azimuth looks',
+                        required=True,
+                        type=int)
+    inps = parser.parse_args()
+
+    return inps
+
+
+def main():
+    inps = cmd_line_parser()
+    slc_dir = inps.slc_dir
+    out_dir = inps.out_dir
+    iw_num = inps.iw_num
+    rlks = inps.rlks
+    alks = inps.alks
+
+    # check slc_dir
+    slc_dir = os.path.abspath(slc_dir)
+    if not os.path.isdir(slc_dir):
+        sys.exit('Cannot find directory {}'.format(slc_dir))
+
+    # check out_dir
+    out_dir = os.path.abspath(out_dir)
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+
+    # get all dates
+    dates = sorted(
+        [i for i in os.listdir(slc_dir) if re.findall(r'^\d{8}$', i)])
+
+    for date in dates:
+        slc_date_dir = os.path.join(slc_dir, date)
+
+        out_date_dir = os.path.join(out_dir, date)
+        if not os.path.isdir(out_date_dir):
+            os.mkdir(out_date_dir)
+
+        tab = os.path.join(out_date_dir, date + '_tab')
+        tab_deramp = os.path.join(out_date_dir, date + '_tab.deramp')
+
+        if len(iw_num) == 1:
+            iw_slc = os.path.join(slc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+
+            iw_slc_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+
+            write_tab(iw_slc, tab)
+
+            write_tab(iw_slc_deramp, tab_deramp)
+
+        if len(iw_num) == 2:
+            iw_slc1 = os.path.join(slc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2 = os.path.join(slc_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+
+            iw_slc1_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+
+            write_tab(iw_slc1, tab)
+            write_tab(iw_slc2, tab)
+
+            write_tab(iw_slc1_deramp, tab_deramp)
+            write_tab(iw_slc2_deramp, tab_deramp)
+
+        if len(iw_num) == 3:
+            iw_slc1 = os.path.join(slc_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2 = os.path.join(slc_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+            iw_slc3 = os.path.join(slc_date_dir, f"{date}.iw{iw_num[2] * 2}.slc")
+
+            iw_slc1_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[0] * 2}.slc")
+            iw_slc2_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[1] * 2}.slc")
+            iw_slc3_deramp = os.path.join(out_date_dir, f"{date}.iw{iw_num[2] * 2}.slc")
+
+            write_tab(iw_slc1, tab)
+            write_tab(iw_slc2, tab)
+            write_tab(iw_slc3, tab)
+
+            write_tab(iw_slc1_deramp, tab_deramp)
+            write_tab(iw_slc2_deramp, tab_deramp)
+            write_tab(iw_slc3_deramp, tab_deramp)
+
+        os.chdir(out_date_dir)
+
+        call_str = f"SLC_deramp_S1_TOPS {tab} {tab_deramp} 0 1"
+        os.system(call_str)
+
+        slc_deramp = os.path.join(out_date_dir, date + ".slc")
+        slc_deramp_par = slc_deramp + '.par'
+
+        call_str = f"SLC_mosaic_S1_TOPS {tab_deramp} {slc_deramp} {slc_deramp_par} {rlks} {alks} 1"
+        os.system(call_str)
+
+        gen_bmp(slc_deramp, slc_deramp_par, rlks, alks)
+
+    print('\nAll done, enjoy it!\n')
+
+
+if __name__ == "__main__":
+    main()
