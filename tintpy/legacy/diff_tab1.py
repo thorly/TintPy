7a1b78d10e200084f2fa6da4112d42a48ac43433
leiyuan <leiyuan1229@outlook.com>
2022-03-24 14:11:58 +0800

    backup old files

---
+++ tintpy/legacy/diff_tab1.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+################################
+# Program is part of TintPy    #
+# Copyright (c) 2021, Lei Yuan #
+# Author: Lei Yuan, 2021       #
+################################
+
+import argparse
+import datetime
+import os
+import re
+import sys
+
+import numpy as np
+
+EXAMPLE = """Example:
+  python3 diff_tab.py /ly/diff 0.5 diff_tab
+  python3 diff_tab.py /ly/diff 0.5 diff_tab -u adf.unw.gacos -c adf.cc.gacos -d diff.par
+"""
+
+
+def cmdline_parser():
+    parser = argparse.ArgumentParser(description='Write diff_tab for Phase-Stacking using GAMMA.',
+        formatter_class=argparse.RawTextHelpFormatter, epilog=EXAMPLE)
+
+    parser.add_argument('diff_dir', help='diff directory including *.unw *.cc and *.diff_par.')
+    parser.add_argument('coh',help='coherence threshold (smaller than this will not be used) for Phase-Stacking.', type=float)
+    parser.add_argument('out_tab', help='diff_tab file for Phase-Stacking.')
+    parser.add_argument('-u', dest='unw_extension', help='filename extension of unwrapped file (defaults: adf.unw).', default='adf.unw')
+    parser.add_argument('-c', dest='cc_extension', help='filename extension of coherence file (defaults: adf.cc).', default='adf.cc')
+    parser.add_argument('-d', dest='diff_par_extension', help='filename extension of diff_par file (defaults: diff_par).', default='diff_par')
+    inps = parser.parse_args()
+
+    return inps
+
+
+def calc_time_delta(pair):
+    """Calculate time delta
+
+    Args:
+        pair (str): date1-date2
+
+    Returns:
+        int: days
+    """
+    dates = re.findall(r'\d{8}', pair)
+    date1 = datetime.datetime.strptime(dates[0], '%Y%m%d')
+    date2 = datetime.datetime.strptime(dates[1], '%Y%m%d')
+    time_delta = (date2 - date1).days
+
+    return time_delta
+
+
+def read_gamma_par(par_file, keyword):
+    """Extract value from par_file using keyword
+
+    Args:
+        par_file (str): GAMMA parameter file
+        keyword (str): keyword like "reange_sample"
+    """
+    value = None
+    with open(par_file, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            if line.count(keyword) == 1:
+                value = line.split()[1].strip()
+
+    return value
+
+
+def read_gamma(file, lines, file_type):
+    """Read GAMMA format file
+
+    Args:
+        file (str): GAMMA format file
+        lines (int): line of file
+        file_type (str): file type
+
+    Returns:
+        array: data
+    """
+    # check file
+    if not os.path.isfile(file):
+        sys.exit('{} does not exist.'.format(file))
+    data = np.fromfile(file, dtype=file_type)
+    # GAMMA output files are big-endian
+    data.byteswap('True')
+    data = data.reshape(lines, -1)
+
+    return data
+
+
+def check_extension(extension):
+    """Check file extension
+
+    Args:
+        extension (str): extension
+
+    Returns:
+        str: extension
+    """
+    if not extension.startswith('.'):
+        extension = '.' + extension
+
+    return extension
+
+
+def get_pairs(diff_dir):
+    """Get all pairs
+
+    Args:
+        diff_dir (str): directory cluding pairs
+
+    Returns:
+        list: pairs
+    """
+    pairs = [i[0:17] for i in os.listdir(diff_dir) if re.match(r'\d{8}_\d{8}', i)]
+    pairs = sorted(list(set(pairs)))
+
+    return pairs
+
+
+def get_diff_par(diff_dir, diff_par_extension):
+    """Get diff_par file
+
+    Args:
+        diff_dir (str): directory contains diff_par
+        diff_par_extension (str): diff_par file extension
+
+    Returns:
+        str: diff_par file path
+    """
+    pairs = get_pairs(diff_dir)
+    diff_par = None
+
+    for pair in pairs:
+        diff_par = os.path.join(diff_dir, pair + diff_par_extension)
+        if os.path.isfile(diff_par):
+            return diff_par
+
+    return diff_par
+
+
+def get_unw_cc(diff_dir, unw_extension, cc_extension):
+    """Get unw and cc file
+
+    Args:
+        diff_dir (str): directory contains unw and cc
+        unw_extension (str): unw extension
+        cc_extension (cc): cc extension
+
+    Returns:
+        tuple: unws and ccs
+    """
+    pairs = get_pairs(diff_dir)
+
+    unws = []
+    ccs = []
+
+    for pair in pairs:
+        unw = os.path.join(diff_dir, pair + unw_extension)
+        cc = os.path.join(diff_dir, pair + cc_extension)
+        if os.path.isfile(unw) and os.path.isfile(cc):
+            unws.append(unw)
+            ccs.append(cc)
+
+    return unws, ccs
+
+
+def get_mean_coh(ccs, diff_par):
+    """Calculate mean coherence
+
+    Args:
+        ccs (list): cc files
+        diff_par (str): diff_par file
+
+    Returns:
+        array: mean coherence array
+    """
+    mean_coh_array = np.zeros(len(ccs))
+
+    length = int(read_gamma_par(diff_par, 'az_samp_1'))
+
+    for cc in ccs:
+        mean_coh = np.mean(read_gamma(cc, length, 'float32'))
+        mean_coh_array[ccs.index(cc)] = mean_coh
+
+    np.save('mean_coh_array', mean_coh_array)
+
+    return mean_coh_array
+
+
+def count_coh(start, end, coh_array):
+    """Count coherence rate in [start end]
+
+    Args:
+        start (float): start point
+        end (float): end point
+        coh_array (array): mean coherence array
+    """
+    num = 0
+    for i in coh_array:
+        if i > start and i <= end:
+            num += 1
+    rate = num / coh_array.shape[0]
+    print(f"{start}~{end}: {round(rate, 2)}")
+
+
+def statistic_coh(mean_coh_array, step=0.05):
+    """Statistic coherence
+
+    Args:
+        mean_coh_array (array): mean coherence array
+        step (float, optional): step. Defaults to 0.05.
+    """
+    for i in np.arange(0, 1, step):
+        start = round(i, 2)
+        end = round(start + step, 2)
+        count_coh(start, end, mean_coh_array)
+
+
+def write_diff_tab(unws, mean_coh_array, coh_thres, tab_file):
+    """Write diff_tab file
+
+    Args:
+        unws (list): unw files
+        mean_coh_array (array): mean coherence array
+        coh_thres (float): coherence threshold
+        tab_file (str): output tab file
+    """
+    print('\nWriting data to {}'.format(tab_file))
+    unw_used = 0
+    with open(tab_file, 'w+') as f:
+        for unw in unws:
+            unw_name = os.path.basename(unw)
+            time_delta = calc_time_delta(unw_name[0:17])
+            coh = mean_coh_array[unws.index(unw)]
+            if coh >= coh_thres:
+                f.write(f"{unw}    {time_delta}\n")
+                unw_used += 1
+
+    rate = round(unw_used / len(unws), 2) * 100
+    print('unwrapped file used: {}/{} [{}%]'.format(unw_used, len(unws), rate))
+
+
+def main():
+    # get inputs
+    inps = cmdline_parser()
+    diff_dir = os.path.abspath(inps.diff_dir)
+    coh_thres = inps.coh
+    tab_file = inps.out_tab
+    unw_extension = inps.unw_extension
+    cc_extension = inps.cc_extension
+    diff_par_extension = inps.diff_par_extension
+
+    # check diff_dir
+    if not os.path.isdir(diff_dir):
+        sys.exit('{} does not exist'.format(diff_dir))
+
+    # check extension
+    unw_extension = check_extension(unw_extension)
+    cc_extension = check_extension(cc_extension)
+    diff_par_extension = check_extension(diff_par_extension)
+
+    # get pairs
+    pairs = get_pairs(diff_dir)
+
+    if len(pairs) < 1:
+        sys.exit('No pair in {}.'.format(diff_dir))
+
+    # get diff_par
+    diff_par = get_diff_par(diff_dir, diff_par_extension)
+
+    if not diff_par:
+        sys.exit('Cannot find diff_par file in {}.'.format(diff_dir))
+
+    # get unws and ccs:
+    unws, ccs = get_unw_cc(diff_dir, unw_extension, cc_extension)
+
+    if len(unws) < 1:
+        sys.exit('No unw file in {}.'.format(diff_dir))
+
+    # statistic coherence
+    print('Statisticing coherence:')
+    if os.path.isfile('mean_coh_array.npy'):
+        mean_coh_array = np.load('mean_coh_array.npy')
+    else:
+        mean_coh_array = get_mean_coh(ccs, diff_par)
+
+    statistic_coh(mean_coh_array)
+
+    # write diff_tab
+    write_diff_tab(unws, mean_coh_array, coh_thres, tab_file)
+
+    print('\nAll done, enjot it!\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ tintpy/legacy/gamma2mintpy1.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+################################
+# Program is part of TintPy    #
+# Copyright (c) 2021, Lei Yuan #
+# Author: Lei Yuan, 2021       #
+################################
+
+import argparse
+import glob
+import os
+import re
+import shutil
+import sys
+
+
+def cmd_line_parser():
+    parser = argparse.ArgumentParser(
+        description='Prepare files for MintPy time series processing.',
+        formatter_class=argparse.RawTextHelpFormatter,epilog=EXAMPLE)
+    parser.add_argument('diff_dir', help='diff directory')
+    parser.add_argument('mli_dir', help='mli directory')
+    parser.add_argument('geo_dir', help='geo directory')
+    parser.add_argument('mintpy_dir', help='output directory')
+    parser.add_argument('-m', dest='mli_extension', default='rmli', help='mli file extension (defaults: rmli)')
+    parser.add_argument('-u', dest='unw_extension', default='adf.unw', help='unw file extension (defaults: adf.unw)')
+    parser.add_argument('-c', dest='cc_extension', default='adf.cc', help='cc file extension (defaults: adf.cc)')
+    inps = parser.parse_args()
+
+    return inps
+
+
+EXAMPLE = """Example:
+  python3 gamma2mintpy2d.py /ly/diff /ly/mli /ly/geo /ly/MintPy
+  python3 gamma2mintpy2d.py /ly/diff /ly/mli /ly/geo /ly/MintPy -u adf.unw.gacos -c adf.cc.gacos
+"""
+
+
+def is_dir(dir):
+    """Check directory
+
+    Args:
+        dir (str): directory
+    """
+    if not os.path.isdir(dir):
+        sys.exit('{} does not exist'.format(dir))
+
+def mk_dir(dir):
+    """Make directory
+
+    Args:
+        dir (str): directory
+    """
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+def get_rlks(mli_par):
+    """Get rlks from par file
+
+    Args:
+        mli_par (str): par file
+
+    Returns:
+        str: rlks
+    """
+    rlks = None
+    with open(mli_par, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            if line.count('range_looks') == 1:
+                rlks = line.split(':')[1].strip()
+
+    return rlks
+
+def check_extension(extension):
+    """Check file extension
+
+    Args:
+        extension (str): extension
+
+    Returns:
+        str: extension
+    """
+    if not extension.startswith('.'):
+        extension = '.' + extension
+
+    return extension
+
+
+def main():
+    # get inputs
+    inps = cmd_line_parser()
+    diff_dir = os.path.abspath(inps.diff_dir)
+    mli_dir = os.path.abspath(inps.mli_dir)
+    geo_dir = os.path.abspath(inps.geo_dir)
+    mintpy_dir = os.path.abspath(inps.mintpy_dir)
+    mli_extension = inps.mli_extension
+    unw_extension = inps.unw_extension
+    cc_extension = inps.cc_extension
+
+    # check
+    is_dir(diff_dir)
+    is_dir(mli_dir)
+    is_dir(geo_dir)
+    mk_dir(mintpy_dir)
+
+    mli_extension = check_extension(mli_extension)
+    unw_extension = check_extension(unw_extension)
+    cc_extension = check_extension(cc_extension)
+
+    # directory for mintpy
+    geometry_dir = os.path.join(mintpy_dir, 'geometry')
+    interferograms_dir = os.path.join(mintpy_dir, 'interferograms')
+    mk_dir(geometry_dir)
+    mk_dir(interferograms_dir)
+
+    # get pairs
+    pairs = [i[0:17] for i in os.listdir(diff_dir) if re.match(r'\d{8}_\d{8}', i)]
+    pairs = sorted(list(set(pairs)))
+
+    if len(pairs) < 1:
+        sys.exit('No pair in {}.'.format(diff_dir))
+
+    # get rlks
+    date = pairs[0][0:8]
+    mli_par = os.path.join(mli_dir, date + mli_extension + '.par')
+    rlks = get_rlks(mli_par)
+
+    print('Copying files into {}'.format(geometry_dir))
+
+    UTM_TO_RDC_path = os.path.join(geo_dir, 'lookup_table_fine')
+    dst_UTM_TO_RDC = 'sim_' + date + '_' + rlks + 'rlks.UTM_TO_RDC'
+    dst_UTM_TO_RDC_path = os.path.join(geometry_dir, dst_UTM_TO_RDC)
+
+    diff_par_path = glob.glob(os.path.join(geo_dir, '*.diff_par'))[0]
+    dst_diff_par = 'sim_' + date + '_' + rlks + 'rlks.diff_par'
+    dst_diff_par_path = os.path.join(geometry_dir, dst_diff_par)
+
+    rdc_dem_path = glob.glob(os.path.join(geo_dir, '*.dem'))[0]
+    dst_rdc_dem = 'sim_' + date + '_' + rlks + 'rlks.rdc.dem'
+    dst_rdc_dem_path = os.path.join(geometry_dir, dst_rdc_dem)
+
+    utm_dem_path = os.path.join(geo_dir, 'dem_seg')
+    dst_utm_dem = 'sim_' + date + '_' + rlks + 'rlks.utm.dem'
+    dst_utm_dem_path = os.path.join(geometry_dir, dst_utm_dem)
+
+    utm_dem_par_path = utm_dem_path + '.par'
+    dst_utm_dem_par = dst_utm_dem + '.par'
+    dst_utm_dem_par_path = os.path.join(geometry_dir, dst_utm_dem_par)
+
+    shutil.copy(UTM_TO_RDC_path, dst_UTM_TO_RDC_path)
+    shutil.copy(diff_par_path, dst_diff_par_path)
+    shutil.copy(rdc_dem_path, dst_rdc_dem_path)
+    shutil.copy(utm_dem_path, dst_utm_dem_path)
+    shutil.copy(utm_dem_par_path, dst_utm_dem_par_path)
+
+    print('Copying files into {}'.format(interferograms_dir))
+
+    for pair in pairs:
+        m_date = pair[0:8]
+        s_date = pair[-8:]
+
+        off = pair + '.off'
+        off_path = os.path.join(diff_dir, off)
+        e1 = os.path.isfile(off_path)
+
+        m_amp_par = m_date + mli_extension + '.par'
+        m_amp_par_path = os.path.join(mli_dir, m_amp_par)
+        e2 = os.path.isfile(m_amp_par_path)
+
+        s_amp_par = s_date + mli_extension + '.par'
+        s_amp_par_path = os.path.join(mli_dir, s_amp_par)
+        e3 = os.path.isfile(s_amp_par_path)
+
+        cor = pair + cc_extension
+        cor_path = os.path.join(diff_dir, cor)
+        e3 = os.path.isfile(cor_path)
+
+        unw = pair + unw_extension
+        unw_path = os.path.join(diff_dir, unw)
+        e4 = os.path.isfile(unw_path)
+
+        if e1 and e2 and e3 and e4:
+            dst_ifg_dir = os.path.join(interferograms_dir, pair)
+            mk_dir(dst_ifg_dir)
+
+            dst_off = pair + '_' + rlks + 'rlks.off'
+            dst_off_path = os.path.join(dst_ifg_dir, dst_off)
+            shutil.copy(off_path, dst_off_path)
+
+            dst_m_amp_par = m_date + '_' + rlks + 'rlks.amp.par'
+            dst_m_amp_par_path = os.path.join(dst_ifg_dir, dst_m_amp_par)
+            shutil.copy(m_amp_par_path, dst_m_amp_par_path)
+
+            dst_s_amp_par = s_date + '_' + rlks + 'rlks.amp.par'
+            dst_s_amp_par_path = os.path.join(dst_ifg_dir, dst_s_amp_par)
+            shutil.copy(s_amp_par_path, dst_s_amp_par_path)
+
+            dst_cor = 'filt_' + pair + '_' + rlks + 'rlks.cor'
+            dst_cor_path = os.path.join(dst_ifg_dir, dst_cor)
+            shutil.copy(cor_path, dst_cor_path)
+
+            dst_unw = 'diff_' + pair + '_' + rlks + 'rlks.unw'
+            dst_unw_path = os.path.join(dst_ifg_dir, dst_unw)
+            shutil.copy(unw_path, dst_unw_path)
+
+    # copy notebook to mintpy_dir
+    try:
+        TINTPY_HOME = os.environ['TINTPY_HOME']
+        notebook1 = os.path.join(TINTPY_HOME, 'tintpy', 'notebook', 'MintPy.ipynb')
+        notebook2 = os.path.join(TINTPY_HOME, 'tintpy', 'notebook', 'read_mintpy.ipynb')
+        shutil.copy(notebook1, mintpy_dir)
+        shutil.copy(notebook2, mintpy_dir)
+    except Exception as e:
+        print(e)
+
+    print('\nAll done, enjoy it!\n')
+
+
+if __name__ == "__main__":
+    main()
